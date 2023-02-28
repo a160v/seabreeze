298ba3ad9f353f4acb10f7e13f32496b60f130a7
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-02-28 16:15:50 +0100

    fixed index html file issue

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -2,28 +2,32 @@ class BoatsController < ApplicationController
   def index
     @boats = Boat.all
   end
+
   def show
     @boat = Boat.find(params[:id])
-
-    # get "/my-boats/", to: "pages#home", constraints: ->(request) {
-    #   boat = Boat.find_by(id: request.params[:boat_id])
-    #   boat && boat.user_id == request.session[:user_id]
+    # if @boat = Boat.find_by(id: request.params[:boat_id])
+    # boat && boat.user_id == request.session[:user_id]
   end
+
   def new
     @boat = Boat.new # Needed to instantiate the form_with
   end
+
   def create
     @boat = Boat.new(boat_params)
     @boat.save # Will raise ActiveModel::ForbiddenAttributesError
   end
+
   def edit
     @boat = Boat.find(params[:id])
   end
+
   def update
     @boat = Boat.find(params[:id])
     @boat.update(boat_params)
     redirect_to boat_path(@boat)
   end
+
   def destroy
     @boat = Boat.find(params[:id])
     @boat.destroy
@@ -31,10 +35,10 @@ class BoatsController < ApplicationController
     redirect_to boats_path, status: :see_other
   end
 
-private
+  private
 
-def boat_params
-  params.require(:boat).permit(:title, :price_per_day)
-end
+  def boat_params
+    params.require(:boat).permit(:title, :price_per_day)
+  end
 
 end
