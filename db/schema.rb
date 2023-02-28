8503969d4fcb7daeaf650db44817a573799579ae
Patrick Schur <patrick.schur@icloud.com>
2023-02-28 16:17:36 +0100

    adfasd

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_111017) do
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
+  create_table "installs", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_installs_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
