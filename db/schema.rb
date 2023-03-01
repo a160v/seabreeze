a2ccd1e08c8d5cc1f62ac7d845ecf901c90fac2b
Patrick Schur <patrick.schur@icloud.com>
2023-03-01 08:47:57 +0100

    my last changes

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_145728) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "installs", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_installs_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -52,4 +64,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_145728) do
   add_foreign_key "boats", "users"
   add_foreign_key "bookings", "boats"
   add_foreign_key "bookings", "users"
- end
+end
