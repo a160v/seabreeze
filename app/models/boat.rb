7f8e55beec41de00e18646ff4dd127572f17311d
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-03-02 15:55:54 +0100

    fixed global search

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,7 +3,7 @@ class Boat < ApplicationRecord
   belongs_to :user, dependent: :destroy
   has_many :bookings
 
-  # Global search
+  # Global search; NOTA BENE: email is exposed for educational purposes.
   pg_search_scope :global_search,
   against: %i[title price_per_day type description capacity address manufacturing_year],
   associated_against: { user: [:email] },
