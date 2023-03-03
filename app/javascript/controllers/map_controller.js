c6cc8d4be29eaf68d9ada98a5e8f111a98001dbf
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-03-03 16:15:02 +0100

    final push

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -7,8 +7,8 @@ export default class extends Controller {
   };
 
   connect() {
+    console.log("We are connected!");
     mapboxgl.accessToken = this.apiKeyValue;
-
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10",
@@ -22,7 +22,17 @@ export default class extends Controller {
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(this.map);
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html);
+
+      // Create a HTML element for your custom marker
+      const customMarker = document.createElement("div");
+      customMarker.innerHTML = marker.marker_html;
+
+      // Pass the element as an argument to the new marker
+      new mapboxgl.Marker(customMarker)
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
+        .addTo(this.map);
     });
   }
 
@@ -32,30 +42,5 @@ export default class extends Controller {
       bounds.extend([marker.lng, marker.lat])
     );
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-  }
-
-  #addMarkersToMap() {
-    this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html) // Add this
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup) // Add this
-        .addTo(this.map)
-    });
-  }
-  #addMarkersToMap() {
-    this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
-
-      // Create a HTML element for your custom marker
-      const customMarker = document.createElement("div")
-      customMarker.innerHTML = marker.marker_html
-
-      // Pass the element as an argument to the new marker
-      new mapboxgl.Marker(customMarker)
-        .setLngLat([marker.lng, marker.lat])
-        .setPopup(popup)
-        .addTo(this.map)
-    })
   }
 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# create 10 users
+5.times do |i|
+  User.create!(
+    email: "user#{i+1}@example.com",
+    password: "password12",
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    # profile_picture: Faker::LoremPixel.image(size: "50x50"),
+    admin: false
+  )
+  puts "user #{i+1} created"
+end
+
+# create 10 boats
+5.times do |i|
+  Boat.create!(
+    title: Faker::JapaneseMedia::OnePiece.sea,
+    price_per_day: rand(1000..10000),
+    user_id: rand(1..5),
+    # type: "yacht",
+    description: Faker::Lorem.paragraph,
+    capacity: rand(1..10),
+    address: "Forellenweg 9, 8038 Zürich",
+    picture: "https://source.unsplash.com/300x200/?boat/",
+    manufacturing_year: rand(2000..2022),
+    with_crew: Faker::Boolean.boolean,
+    availability: Faker::Boolean.boolean,
+  )
+  puts "boat #{i+1} created"
+end
+
+# create 10 bookings
+10.times do |i|
+  Booking.create!(
+    check_in: Faker::Date.forward(days: 10),
+    check_out: Faker::Date.forward(days: 20),
+    boat_id: rand(1..5),
+    user_id: rand(1..5),
+    booking_price: rand(500..5000),
+    status: 'pending'
+  )
+  puts "booking #{i+1}created"
+end
